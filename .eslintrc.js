bda1b3e908615d4d6b4ea44d07605005ff8e3f01
ian <ian.lee@healingpaper.com>
2021-07-04 20:47:33 +0900

    init setting and set lint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:react-hooks/recommended', // React hooks rules
+    'plugin:prettier/recommended', // Prettier recommended rules
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': 'off', // We will use TypeScript's types for component props instead
+    'react/react-in-jsx-scope': 'off', // No need to import React with Next.js
+    'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with how Next.js's <Link />
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-function-return-type': [
+      // I suggest this setting for requiring return types on functions only where usefull
+      'warn',
+      {
+        allowExpressions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+      },
+    ],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }, { usePrettierrc: true }], // Includes .prettierrc.js rules
+    '@typescript-eslint/no-empty-interface': 0, // default prop를 정의하기 위함
+    '@typescript-eslint/no-empty-function': 0, // empty function을 허용함
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useRecoilCallback',
+      },
+    ],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "arrowParens": "avoid", // 화살표 함수 괄호 사용 방식
+  "bracketSpacing": true, // 객체 리터럴에서 괄호에 공백 삽입 여부 
+  "htmlWhitespaceSensitivity": "css", // HTML 공백 감도 설정
+  "insertPragma": false, // 미리 정의된 @format marker의 사용 여부 (v1.8.0)
+  "jsxBracketSameLine": false, // JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+  "jsxSingleQuote": true, // JSX에 single 쿼테이션 사용 여부
+  "printWidth": 100, // 줄 바꿈 폭
+  "quoteProps": "as-needed", // 객체 속성 쿼테이션 적용 방식
+  "requirePragma": false, // 파일 상단에 미리 정의된 주석을 작성하고 Pragma로 포맷팅 사용 여부 지정
+  "semi": false, // 세미콜론 사용 여부
+  "singleQuote": true, // single 쿼테이션 사용 여부
+  "tabWidth": 2, // 탭 너비
+  "trailingComma": "all", // 후행 콤마 사용
+}
