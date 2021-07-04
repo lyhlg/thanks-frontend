b2da9d9da3796fe97b3a357829220dd12248a091
ian <ian.lee@healingpaper.com>
2021-07-05 08:48:23 +0900

    added recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from 'react-router-dom'
 import { IonApp, IonRouterOutlet } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
+import { RecoilRoot } from 'recoil'
 import Home from './pages/Home'
 
 /* Core CSS required for Ionic components to work properly */
@@ -23,18 +24,20 @@ import '@ionic/react/css/display.css'
 import './theme/variables.css'
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path='/home'>
-          <Home />
-        </Route>
-        <Route exact path='/'>
-          <Redirect to='/home' />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+  <RecoilRoot>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path='/home'>
+            <Home />
+          </Route>
+          <Route exact path='/'>
+            <Redirect to='/home' />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  </RecoilRoot>
 )
 
 export default App
